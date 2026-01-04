1a7039de8f871ef6cfc5e7eda5e6c0a2184aedea
justanamir <amirdanialmuslim@gmail.com>
2026-01-04 18:41:40 +0800

    Day 2; defined models. Left some data out such as race, religion out for now to keep it simple & not sure yet what use I can get from it. Did not add patient in UserRole for now, might be a good idea to add them.

---
+++ domain/model.go
@@ -0,0 +1,58 @@
+package domain
+
+import "time"
+
+type Patient struct {
+	ID          string
+	Fullname    string
+	NRIC        string
+	DateOfBirth time.Time
+	Sex         string
+
+	Phone   string
+	Email   string
+	Address string
+
+	CreatedAt time.Time
+}
+
+// Visit represents a single clinic encounter.
+type Visit struct {
+	ID        string
+	PatientID string
+
+	VisitTime time.Time
+	Reason    string
+
+	DoctorID string
+	Notes    MedicalNote
+
+	CreatedAt time.Time
+}
+
+// MedicalNote represents clinical documentation for a visit.
+type MedicalNote struct {
+	Subjective string
+	Objective  string
+	Assessment string
+	Plan       string
+}
+
+// User represents clinic staff.
+type User struct {
+	ID       string
+	FullName string
+	Role     UserRole
+
+	Active bool
+}
+
+// UserRole defines staff roles.
+type UserRole string
+
+const (
+	RoleDoctor       UserRole = "doctor"
+	RoleNurse        UserRole = "nurse"
+	RoleReceptionist UserRole = "receptionist"
+	RoleAdmin        UserRole = "admin"
+)
